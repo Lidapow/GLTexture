613f2406aed4bfb4f269440897c4cb9ca990d8dc
Lidapow <Lidapow@users.noreply.github.com>
2015-08-24 22:19:57 +0800

    Use double buffer instead of three buffer

--- src/Decoder.cpp
+++ src/Decoder.cpp
@@ -4,20 +4,16 @@
 
 Decoder::Decoder () {
 	m_BufferIndex = 0;
-	// m_DecodedLen = len;
-	// m_DecodedData = new char[100];
-	// memset(m_DecodedData, 0, sizeof(int) * 2);
 }
 
 Decoder::~Decoder () {
 	delete[] m_Buffer[0];
 	delete[] m_Buffer[1];
-	delete[] m_TempBuffer;
 	m_BufferIndex = 0;
 }
 
 int Decoder::GetDecodedLen() {
-	return m_BufferLen[m_BufferIndex];
+	return m_BufferLen;
 }
 
 char* Decoder::GetDecodedData() {
@@ -28,47 +24,42 @@ void Decoder::SetDimension (int width, int height) {
 	m_Width = width;
 	m_Height = height;
 	m_BufferIndex = 0;
-	m_TempFilled = 0;
+	m_BufferFilled = 0;
 
 	int len = m_Width * m_Height * 3;
-	m_TempBufferLen = m_Width * m_Height * 3;
-	m_BufferLen[0] = len;
-	m_BufferLen[1] = len;
+	m_BufferLen = len;
 	m_Buffer[0] = new char[len];
 	m_Buffer[1] = new char[len];
-	m_TempBuffer = new char[m_TempBufferLen];
 	memset(m_Buffer[0], 0, len);
 	memset(m_Buffer[1], 0, len);
-	memset(m_TempBuffer, 0, m_TempBufferLen);
 }
 
 void Decoder::Decode (int dataLen, char* data) {
-	int index = (m_BufferIndex + 1) % 2;
+	char* workingBuff = m_Buffer[(m_BufferIndex + 1) % 2];
 	int right = 0, left = 0;
-	if(dataLen + m_TempFilled <= m_TempBufferLen) {
-		memcpy(&m_TempBuffer[m_TempFilled], data, dataLen);
-		m_TempFilled += dataLen;
-	} else {
-		//Temporary use remain
-		left = m_TempBufferLen - m_TempFilled;
-		right = (dataLen + m_TempFilled) - m_TempBufferLen;
 
-		memcpy(&m_TempBuffer[m_TempFilled], data, left);
-		m_TempFilled += left;
+	if(dataLen + m_BufferFilled <= m_BufferLen) {
+		memcpy(&workingBuff[m_BufferFilled], data, dataLen);
+		m_BufferFilled += dataLen;
+	} else {
+		left = m_BufferLen - m_BufferFilled;
+		right = (dataLen + m_BufferFilled) - m_BufferLen;
+
+		memcpy(&workingBuff[m_BufferFilled], data, left);
+		m_BufferFilled += left;
 	}
 
-	if(m_TempFilled < m_TempBufferLen)
+	if(m_BufferFilled < m_BufferLen)
 		return;
 
-	memcpy(m_Buffer[index], m_TempBuffer, m_TempBufferLen);
-
 	if(right > 0) {
-		memcpy(m_TempBuffer, &data[dataLen - right], right);
-		m_TempFilled = right;
+		workingBuff = m_Buffer[m_BufferIndex];
+		memcpy(workingBuff, &data[dataLen - right], right);
+		m_BufferFilled = right;
 	} else {
-		m_TempFilled = 0;
+		m_BufferFilled = 0;
 	}
-	// memcpy(m_Buffer[index], data, dataLen);
+
 	m_BufferIndex = (m_BufferIndex + 1) % 2;
 	return;
 }
@@ -76,8 +67,7 @@ void Decoder::Decode (int dataLen, char* data) {
 void Decoder::Release () {
 	delete[] m_Buffer[0];
 	delete[] m_Buffer[1];
-	delete[] m_TempBuffer;
 	m_BufferIndex = 0;
-	m_TempFilled = 0;
+	m_BufferFilled = 0;
 }
 
--- src/Decoder.h
+++ src/Decoder.h
@@ -15,11 +15,8 @@ class Decoder {
 		int m_Width;
 		int m_Height;
 
-		int m_TempBufferLen;
-		char* m_TempBuffer;
-		int m_TempFilled;
-
-		int m_BufferLen[2];
+		int m_BufferLen;
+		int m_BufferFilled;
 		char* m_Buffer[2];
 };
 
